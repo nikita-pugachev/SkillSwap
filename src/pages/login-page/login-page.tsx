295c8700c5fd2575f1ab31d52da68c065b857a7c
margarita <tmskrarr@gmail.com>
2026-03-14 11:58:08 +0500

    feat: add login page layout

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,3 +1,20 @@
+import styles from './login-page.module.scss';
+
 export default function LoginPage() {
-  return <div>Login Page (stub)</div>;
+  return (
+    <div className={styles.page}>
+      <header className={styles.header}>
+        <div>SkillSwap</div>
+        <div>Закрыть</div>
+      </header>
+
+      <h1 className={styles.title}>Вход</h1>
+
+      <main className={styles.content}>
+        <section className={styles.form}>form</section>
+
+        <section className={styles.onboarding}>onboarding</section>
+      </main>
+    </div>
+  );
 }
