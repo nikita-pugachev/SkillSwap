465fe6b42fa07c1a41ebf37d7c00cfdac1ec33ce
Sofia Lesnikova <sooofiawork@gmail.ru>
2026-03-18 11:01:05 +0300

    fix: clear comment and link

--- src/components/ui/CardSectionUI/CardSectionUI.tsx
+++ src/components/ui/CardSectionUI/CardSectionUI.tsx
@@ -1,45 +1,38 @@
 import styles from './CardSectionUI.module.scss';
 import Icon from '../../../assets/icons/chevron-right.svg?react';
-import { Button } from '../ButtonUI';
+import { Link } from 'react-router-dom';
 
-// import {UserCard} from '../UserCard'
+// TODO: добавить import UserCard
 
+// TODO: добавить поле cards: User[]
 export interface UsersSectionProps {
   title: string;
   buttonText: string;
-  //   cards: []; //User[]
-  onButtonClick: () => void;
+  showAllLink?: string;
 }
 
-export const CardSectionUI = ({
-  title,
-  buttonText,
-  // cards,
-  onButtonClick,
-}: UsersSectionProps) => {
+export const CardSectionUI = ({ title, buttonText, showAllLink }: UsersSectionProps) => {
   return (
     <section className={styles.cardSection}>
       <div className={styles.textContainer}>
-        {/* Вот тут вот очень странно, по стилям у нас стили для h1 и в макете 
-                и в переменных, но как бы тогда семантически неверно, так как несколько 
-                h1 на странице. скажите на что заменить)) */}
-        <h1 className={styles.title}>{title}</h1>
-        <Button onClick={onButtonClick} variant="tertiary" className={styles.button}>
-          <span className={styles.buttonContent}>
-            <span>{buttonText}</span>
-            <Icon className={styles.icon} />
+        <h2 className={styles.title}>{title}</h2>
+        {showAllLink ? (
+          <Link to={showAllLink} className={styles.showAll}>
+            <span className={styles.buttonContent}>
+              <span>{buttonText}</span>
+              <Icon className={styles.icon} />
+            </span>
+          </Link>
+        ) : (
+          <span className={styles.showAll}>
+            <span className={styles.buttonContent}>
+              <span>{buttonText}</span>
+              <Icon className={styles.icon} />
+            </span>
           </span>
-        </Button>
+        )}
       </div>
-      <div className={styles.cardContainer}>
-        {/* {cards.map((user) =>
-          рендер карточек, поправлю для правильного отображения, но условно вот так
-              <UserCard
-              key={user.id}
-              user={user}
-              />
-        )} */}
-      </div>
+      <div className={styles.cardContainer}>{/* TODO: добавить рендер UserCard */}</div>
     </section>
   );
 };
