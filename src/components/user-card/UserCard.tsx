c79e26be8f9a9c699a2186ebef2fef90ba3e7510
Danil Kholkin <danil.kholk1n@yandex.ru>
2026-03-19 20:59:06 +0500

    feat(connect-favorites-to-usercard): connect favorites toggle to UserCard heart button

--- src/components/user-card/UserCard.tsx
+++ src/components/user-card/UserCard.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { SkillTag } from '@/components/ui/SkillTag';
 import { Button } from '@/components/ui/ButtonUI';
 import { Avatar } from '@/components/ui/Avatar';
@@ -7,6 +8,9 @@ import likeOutline from '@/assets/icons/like-outline.svg';
 import likeFilled from '@/assets/icons/like-filled.svg';
 import { SkillCategorySlug } from '@/utils/types';
 import styles from './UserCard.module.scss';
+import { toggleFavorite } from '@/services/slices/favoritesSlice';
+import { selectIsFavorite } from '@/services/selectors';
+import { RootState } from '@/services/store';
 
 export type UserSkill = {
   name: string;
@@ -21,8 +25,6 @@ export interface UserCardProps {
   birthday: string;
   skillsTeach: UserSkill[];
   skillsLearn: UserSkill[];
-  isFavorite: boolean;
-  onFavoriteToggle: (id: string | number) => void;
   onDetailsClick: (id: string | number) => void;
 }
 
@@ -34,10 +36,12 @@ export const UserCard: React.FC<UserCardProps> = ({
   birthday,
   skillsTeach,
   skillsLearn,
-  isFavorite,
-  onFavoriteToggle,
   onDetailsClick,
 }) => {
+  const dispatch = useDispatch();
+  const userId = Number(id);
+  const isFavorite = useSelector((state: RootState) => selectIsFavorite(userId)(state));
+
   const calculateAge = (birthDate: string): number => {
     const today = new Date();
     const birth = new Date(birthDate);
@@ -74,7 +78,6 @@ export const UserCard: React.FC<UserCardProps> = ({
     );
   };
 
-  const handleFavoriteClick = () => onFavoriteToggle(id);
   const handleDetailsClick = () => onDetailsClick(id);
 
   return (
@@ -93,7 +96,7 @@ export const UserCard: React.FC<UserCardProps> = ({
         <IconButton
           iconSrc={isFavorite ? likeFilled : likeOutline}
           ariaLabel={isFavorite ? 'Удалить из избранного' : 'Добавить в избранное'}
-          onClick={handleFavoriteClick}
+          onClick={() => dispatch(toggleFavorite(userId))}
           className={styles.favoriteButton}
         />
       </div>
