9c5016d3af01f75d3b3746c74c2a044b2461ed7c
Sofia Lesnikova <sooofiawork@gmail.ru>
2026-03-24 09:58:43 +0300

    fix: fix style and path

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -11,4 +11,4 @@ export { InputBaseContainerUI } from './InputBaseContainerUI';
 export { FormInputUI } from './FormInputUI';
 export { ErrorPageUI } from './ErrorPageUI';
 export { DateInputUI } from './DateInputUI';
-export { MultiSelectInputUI } from './MultiSelectInputUI.tsx';
+export { MultiSelectInputUI } from './MultiSelectInputUI';
